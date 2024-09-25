67dbe566f753d96d1ad6071ad2769710d4cbd313
Fabio <Fabio@ada-school.com>
2024-09-24 23:53:54 -0300

    agregada seguridad con jwt

--- src/main/java/com/api/school/entity/UserEntity.java
+++ src/main/java/com/api/school/entity/UserEntity.java
@@ -6,12 +6,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Document(collection="users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     private String id;
     private String name;
@@ -19,4 +24,34 @@ public class UserEntity {
 
     private String password;
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
